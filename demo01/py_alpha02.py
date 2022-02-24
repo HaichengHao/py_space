55e15c047f954e2a9c4f6b42844f340e8aab3f35
hero <1326172435@qq.com>
2022-02-24 17:38:23 +0800

    python学习第1,2节

--- demo01/py_alpha02.py
+++ demo01/py_alpha02.py
@@ -29,3 +29,30 @@ print(int(f1),type(int(f1)))
 124 <class 'int'>
 98 <class 'int'>
 '''
+# 转换为float类型，需要float()
+
+s2 = '128.98'
+s3 = '76'
+ff = True
+fff = 'hello'
+i  = 98
+print(type(s2),type(s3),type(ff),type(fff),type(i))
+# print(type(float(s2),float(s3),float(ff),float(fff),float(i)))
+'''--结果--
+Traceback (most recent call last):
+  File "D:\py_space\demo01\py_alpha02.py", line 40, in <module>
+    print(type(float(s2),float(s3),float(ff),float(fff),float(i)))
+ValueError: could not convert string to float: 'hello'
+可知非数字的字符串不允许转换成float类型'''
+#注释掉错误测试
+print(float(s2),float(s3),float(ff),float(i))
+'''--结果--
+<class 'str'> <class 'str'> <class 'bool'> <class 'str'> <class 'int'>
+128.98 76.0 1.0 98.0
+'''
+
+#python中的注释
+# 以’#‘号开头，知道换行结束
+# 以''' '''一对三引号之间的内容多行注释
+# 声明注释，在文件开头加上声明注释，如’#coding:gbk (以gbk为编码格式)#coding:utf-8(以utf-8为编码格式)‘
+
